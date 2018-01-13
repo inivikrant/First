cc34f0ab79614e4db5abe108a306eb9ad22ab0a8
Vikrant Jain <inivikrant@gmail.com>
2018-01-13 17:04:46 +0530

    changes to dat-util

--- date-util.py
+++ date-util.py
@@ -2,3 +2,9 @@
 
 # a function that will return the current year
 
+# a function that will return the timestamp
+
+# a function that will return the currentmonth
+
+# a function that will help me return the current day of week
+
